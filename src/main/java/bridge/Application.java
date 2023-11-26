abf3787ee9b438b9b298609be0f5f733ac346fc5
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-26 17:19:58 +0900

    feat: Application 에서 실질적으로 구동 가능하도록 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,29 @@
 package bridge;
 
+import bridge.controller.BridgeGame;
+import bridge.controller.BridgeGameController;
+import bridge.utils.BridgeMaker;
+import bridge.utils.BridgeRandomNumberGenerator;
+import bridge.view.input.ConsoleReader;
+import bridge.view.input.InputView;
+import bridge.view.input.Reader;
+import bridge.view.output.ConsolePrinter;
+import bridge.view.output.OutputView;
+import bridge.view.output.Printer;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Reader reader = new ConsoleReader();
+        Printer printer = new ConsolePrinter();
+
+        InputView inputView = new InputView(reader, printer);
+        OutputView outputView = new OutputView(printer);
+
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        BridgeGame bridgeGame = new BridgeGame(bridgeMaker);
+
+        BridgeGameController controller = new BridgeGameController(bridgeGame, inputView, outputView);
+        controller.run();
     }
 }
