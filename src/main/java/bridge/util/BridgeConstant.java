2d7b0ab1464c5a01d5273766fbd71be78555d516
jinchiim <nellroll@syuin.ac.kr>
2023-11-26 17:51:05 +0900

    feat(BridgeConstnat.java): 상수 클래스 구현
    - 공통으로 사용하는 상수 클래스 선언

---
+++ src/main/java/bridge/util/BridgeConstant.java
@@ -0,0 +1,12 @@
+package bridge.util;
+
+public class BridgeConstant {
+
+    public static final String RESTART_GAME = "R";
+    public static final String QUIT_GAME = "Q";
+    public static final String SELECT_UP = "U";
+    public static final String SELECT_DOWN = "D";
+    public static final String BLANK = " ";
+    public static final String SUCCEED_SYMBOL = "O";
+    public static final String FAIL_SYMBOL = "X";
+}
