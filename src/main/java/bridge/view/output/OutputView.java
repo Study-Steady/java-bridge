9f3231fd023a501db9306f148deca1922bf331a3
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-26 14:08:12 +0900

    feat: OutputView 기능 구현
    실행 결과에 알맞게 문자열을 출력하도록 구현

--- src/main/java/bridge/view/output/OutputView.java
+++ src/main/java/bridge/view/output/OutputView.java
@@ -1,5 +1,7 @@
 package bridge.view.output;
 
+import static bridge.view.IOMessage.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -11,12 +13,17 @@ public class OutputView {
         this.printer = printer;
     }
 
+    public void printWelcomeMessage() {
+        printer.print(WELCOME.getMessage());
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(String map) {
+        printer.print(map);
     }
 
     /**
@@ -24,6 +31,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String map, String result, int movingCount) {
+        printer.print(FINAL_GAME_RESULT.getMessage());
+        printer.print(map);
+        printer.printEmptyLine();
+
+        printer.print(IS_SUCCESS.getMessage() + ": " + result);
+        printer.print(TOTAL_MOVING_COUNT.getMessage() + ": " + movingCount);
     }
 }
