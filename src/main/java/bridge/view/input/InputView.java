4f0453ef046c55c115db102565e96ea414b98d94
jinchiim <nellroll@syuin.ac.kr>
2023-11-26 17:52:51 +0900

    feat(InputView.java): 입력 View 구현
    - 입력을 받는 View 구현

--- src/main/java/bridge/view/input/InputView.java
+++ src/main/java/bridge/view/input/InputView.java
@@ -1,4 +1,9 @@
-package bridge;
+package bridge.view.input;
+
+import bridge.view.input.validator.BridgeSelectValidator;
+import bridge.view.input.validator.BridgeSizeValidator;
+import bridge.view.input.validator.GameEndValidator;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,20 +14,20 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        return new BridgeSizeValidator().validate(Console.readLine());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        return new BridgeSelectValidator().validate(Console.readLine());
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        return new GameEndValidator().validate(Console.readLine());
     }
 }
