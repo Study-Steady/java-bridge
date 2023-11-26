1600b2d66675fc1bac5c986a687132aa6237cb09
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-26 17:52:26 +0900

    feat: 잘못된 입력이 감지되면 예외를 출력하고 해당 부분부터 재입력 받도록 구현

--- src/main/java/bridge/view/input/InputView.java
+++ src/main/java/bridge/view/input/InputView.java
@@ -2,6 +2,12 @@ package bridge.view.input;
 
 import bridge.view.output.Printer;
 
+import java.util.Objects;
+
+import static bridge.exception.ExceptionMessage.*;
+import static bridge.model.BridgeConstant.MAX_BRIDGE_SIZE;
+import static bridge.model.BridgeConstant.MIN_BRIDGE_SIZE;
+import static bridge.model.BridgeSymbol.*;
 import static bridge.utils.StringConvertor.convertStringToInteger;
 import static bridge.view.IOMessage.*;
 
@@ -9,6 +15,7 @@ import static bridge.view.IOMessage.*;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    // todo while 문을 사용하지 않고 리팩토링을 진행
 
     private final Reader reader;
     private final Printer printer;
@@ -22,17 +29,44 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        printer.print(INPUT_BRIDGE_SIZE.getMessage());
-        String input = reader.inputOneLine();
-        return convertStringToInteger(input);
+        boolean flag = true;
+        int size = 0;
+        while (flag) {
+            flag = false;
+            try {
+                printer.print(INPUT_BRIDGE_SIZE.getMessage());
+                String input = reader.inputOneLine();
+                validateInput(input);
+                validateIsDigit(input);
+                size = convertStringToInteger(input);
+                validateBridgeSizeInRange(size);
+            } catch (IllegalArgumentException e) {
+                printer.print(e.getMessage());
+                flag = true;
+            }
+        }
+        return size;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        printer.print(INPUT_MOVING.getMessage());
-        String input = reader.inputOneLine();
+        boolean flag = true;
+        String input = "";
+
+        while (flag) {
+            flag = false;
+            try {
+                printer.print(INPUT_MOVING.getMessage());
+                input = reader.inputOneLine();
+                validateInput(input);
+                validateMovingCommand(input);
+            } catch (IllegalArgumentException e) {
+                printer.print(e.getMessage());
+                flag = true;
+            }
+        }
         return input;
     }
 
@@ -40,8 +74,50 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        printer.print(INPUT_RESTART_OR_QUIT.getMessage());
-        String input = reader.inputOneLine();
+        boolean flag = true;
+        String input = "";
+        while (flag) {
+            flag = false;
+            try {
+                printer.print(INPUT_RESTART_OR_QUIT.getMessage());
+                input = reader.inputOneLine();
+                validateInput(input);
+                validateRestartOrQuitCommand(input);
+            } catch (IllegalArgumentException e) {
+                printer.print(e.getMessage());
+                flag = true;
+            }
+        }
         return input;
+    }
+
+    private void validateInput(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(INVALID_INPUT.getMessage());
+        }
+    }
+
+    private void validateIsDigit(String input) {
+        if (!input.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException(DIGIT_ONLY.getMessage());
+        }
+    }
+
+    private void validateBridgeSizeInRange(int size) {
+        if (size < MIN_BRIDGE_SIZE.getValue() || size > MAX_BRIDGE_SIZE.getValue()) {
+            throw new IllegalArgumentException(INVALID_BRIDGE_SIZE.getMessage());
+        }
+    }
+
+    private void validateMovingCommand(String input) {
+        if (!Objects.equals(input, UP.getSymbol()) && !Objects.equals(input, DOWN.getSymbol())) {
+            throw new IllegalArgumentException(INVALID_MOVING.getMessage());
+        }
+    }
+
+    private void validateRestartOrQuitCommand(String input) {
+        if (!Objects.equals(input, RESTART.getSymbol()) && !Objects.equals(input, QUIT.getSymbol())) {
+            throw new IllegalArgumentException(INVALID_RESTART_OR_QUIT.getMessage());
+        }
     }
 }
