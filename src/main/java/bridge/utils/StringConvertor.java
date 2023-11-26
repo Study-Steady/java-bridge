19b1722c01740a44da58f8856b94259d55572bbc
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-26 14:04:32 +0900

    feat: StringConvertor 정의
    문자열 관련 유틸 기능을 제공하는 클래스 정의, 문자열 ->숫자로 바꿔주는 메서드 정의

---
+++ src/main/java/bridge/utils/StringConvertor.java
@@ -0,0 +1,10 @@
+package bridge.utils;
+
+public class StringConvertor {
+    private StringConvertor() {
+    }
+
+    public static int convertStringToInteger(String input) {
+        return Integer.parseInt(input);
+    }
+}
